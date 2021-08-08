b8226709ea4eedc005c5b2e18990334085e2fd81
Adib <aouadi.med.adib@gmail.com>
2021-08-08 22:21:27 +0100

    Adding the additional lines of code to the app.js for fixing the merge

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const bodyParser = require('body-parser');
 const agencyRoute = require('./routes/agency.js');
 const lineRoute = require('./routes/line.js');
 const stopRoute = require('./routes/stop.js');
-<<<<<<< HEAD
 const itineraryRoute = require('./routes/itenrary.js');
-=======
->>>>>>> 469362630107b133fa7235dc364172faa8b7d16b
 const db = require("./config/db.config.js");
 
 const app = express();
@@ -16,9 +13,6 @@ app.use(bodyParser.json());
 app.use('/agency' , agencyRoute) ; 
 app.use('/line' , lineRoute) ; 
 app.use('/stop' , stopRoute) ; 
-<<<<<<< HEAD
 app.use('/itinerary' , itineraryRoute) ; 
-=======
->>>>>>> 469362630107b133fa7235dc364172faa8b7d16b
 
 app.listen(process.env.PORT || 3000, console.log('Server running correctly'));
