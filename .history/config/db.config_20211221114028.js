413b6c3a5c1c5bd48b45cff8ce7bb52295efa292
Adib <aouadi.adib@gmail.com>
2021-12-21 11:59:52 +0100

    env configuration added

--- .history/config/db.config_20211221114028.js
+++ .history/config/db.config_20211221114028.js
@@ -1,7 +1,6 @@
-const dotenv = require('dotenv');
-dotenv.config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {
+
+mongoose.connect('process.env.MONGO_URI', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
--- .history/config/db.config_20211221115835.js
+++ .history/config/db.config_20211221115835.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {
+console.log(process.env.MONGO_URI);
+mongoose.connect('mongodb+srv://Adib:DAASCloud1;;@cluster0.ifw4d.mongodb.net/srtj?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
