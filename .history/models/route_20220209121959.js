14ca3e1aa34fbfd5c785be841aa89ab58aed42e9
Aouadi Mohamed Adib <aouadi.adib@gmail.com>
2022-03-23 15:06:07 +0100

    .history directory deleted

--- .history/models/route_20220209121959.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const routeSchema = mongoose.Schema({
-  routeNumber: Number,
-  routeName: String,
-  routeType: Number,
-});
-
-module.exports = mongoose.model("Route", routeSchema);
